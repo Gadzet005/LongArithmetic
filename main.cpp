b1f1e6d635efcac2d5efd1fdbe4bb87894375551
Gadzet <saygitovg@gmail.com>
2024-01-30 13:01:01 +0300

    возможность тестирования вручную

--- main.cpp
+++ main.cpp
@@ -3,26 +3,28 @@
 #include "long_number.hpp"
 
 int main() {
-    LongNumber a, b;
-    char op;
-    cin >> a >> op >> b;
+    while (true) {
+        LongNumber a, b;
+        char op;
+        cin >> a >> op >> b;
 
-    switch (op) {
-    case '+':
-        cout << a + b << endl;
-        break;
-    case '-':
-        cout << a - b << endl;
-        break;
-    case '*':
-        cout << a * b << endl;
-        break;
-    case '/':
-        cout << a / b << endl;
-        break;
-    default:
-        throw runtime_error("Доступные операции: + - * /");
-        break;
+        switch (op) {
+        case '+':
+            cout << a + b << endl;
+            break;
+        case '-':
+            cout << a - b << endl;
+            break;
+        case '*':
+            cout << a * b << endl;
+            break;
+        case '/':
+            cout << a / b << endl;
+            break;
+        default:
+            throw runtime_error("Доступные операции: + - * /");
+            break;
+        }
     }
 
     return 0;
