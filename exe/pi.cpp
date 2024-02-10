9c410b1e1d94ec9b29bb6adf492563c58b40d073
Gadzet <saygitovg@gmail.com>
2024-02-10 13:29:21 +0300

    Тест и расчет времени

--- exe/pi.cpp
+++ exe/pi.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
 
 #include "utils.hpp"
 
@@ -16,10 +17,13 @@ int main() {
     piFile.close();
 
     const LongNumber pi(piString, prec);
+
+    const clock_t start = clock();
     LongNumber myPi = calculatePi(prec);
+    const clock_t end = clock();
 
     cout << "Рассчитанное пи:\n" << myPi << endl;
     cout << "Реальное пи:\n" << pi << endl;
     cout << "Разница:\n" << myPi - pi << endl;
-
+    cout << "Время: " << 1000.0 * (end - start) / CLOCKS_PER_SEC << "ms" << endl;
 }
